5b41457f19a2d671006a6d02637963ecd033d6be
Go7hic <gtfx0209@qq.com>
2015-10-24 03:28:02 +0800

    1024

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 #! /usr/bin/env node
 var request = require('request');
 var colors = require('colors');
-// var prettify = function (data) {
-//     var i, temp, word = data.query;
-//       console.log('\n ' + word.bold);
-//       if (data.basic != null) {
-//         var us = typeof data.basic['us-phonetic'] === 'string' ? '美音: [' + data.basic['us-phonetic'] + ']' : '',
-//             uk = typeof data.basic['uk-phonetic'] === 'string' ? '英音: [' + data.basic['uk-phonetic'] + ']' : '';
-//         if (uk && us) {
-//           console.log('\n ' + us + '   ' + uk);
-//         } else if (typeof data.basic['phonetic'] == 'string') {
-//           console.log('\n ' + '拼音'.bold.underline + '：[' + data.basic['phonetic'] + ']');
-//         }
-//         console.log('\n ' + '翻译'.bold.underline + '：' + data.basic['explains'] + '\n');
-//       } else if (data.web != null) {
-//         console.log('\n ' + '网络释义:'.bold.underline);
-
-//         for (i = 0; i < data.web.length; i++) {
-//           temp = data.web[i];
-//           console.log('\n  ' + (i+1) + '. ' + temp.key);
-//           console.log('\n  ' + temp.value);
-//         }
-
-//       } else {
-//         console.log('\n ' + '暂无释义.'.underline);
-//       }
-// }
 /**
  * 美化 api 返回的数据，让在命令行上显示更好看
  * @param  {JSON} data 返回的结果
